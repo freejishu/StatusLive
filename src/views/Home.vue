530780cdc5c188b98108118eecedea315cdb6796
freejishu <i@freejishu.com>
2021-09-10 11:44:06 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
       <el-row :gutter="12">
         <el-col :span="8">
           <el-card shadow="always">
-            总是显示
+            总是显示1
           </el-card>
         </el-col>
         <el-col :span="8">
