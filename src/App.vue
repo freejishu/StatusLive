4de0ef0fb39c4187ff63c470761f3f49d9081083
freejishu <i@freejishu.com>
2021-09-10 11:42:58 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template >
+  <div id="app" style="">
+
+    <!-- Nav -->
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +13,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
+
+body{
+  background: #f5f5f5;
+}
+
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-container class="container_max">
+    <el-header>
+      <h2 class="title">
+        <span class="title_big">状态监控</span>
+        &nbsp;&nbsp;
+        <span style="font-size:1.7rem" id="title_small">StatusLive</span>
+      </h2>
+    </el-header>
+    <el-main>
+      <el-row :gutter="12">
+        <el-col :span="8">
+          <el-card shadow="always">
+            总是显示
+          </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card shadow="hover">
+            鼠标悬浮时显示
+          </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card shadow="never">
+            从不显示
+          </el-card>
+        </el-col>
+      </el-row>
+      <el-button>默认按钮</el-button>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<style>
+.container_max{
+  width: 95%;
+  max-width: 1200px;
+  margin:auto;
+}
+
+.title_big{
+  font-size:2.5rem;
+
+}
+
+.title{
+  text-align: left;
+}
+
+</style>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Index',
+
+}
+</script>
