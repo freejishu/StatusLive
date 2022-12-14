4523046c5cd240adb01c5eae39c0d51febd42d91
freejishu <i@freejishu.com>
2022-12-15 03:07:05 +0800

    fix bugs

--- _worker.js
+++ _worker.js
@@ -1,8 +1,43 @@
-const api_key = 'ur609264-e190f91e2cae9f70e3e9b201';//你的apikey
-
-const cache_time = 10;//缓存时间（秒）
-
+let api_key = '';//你的apikey
+let cache_time = 10;//缓存时间（秒）
 const url = 'https://api.uptimerobot.com/v2/getMonitors';
+
+export default {
+    async fetch(request, env) {
+        api_key = env.api_key ;
+        cache_time = env.cache_time ? env.cache_time : 10;
+        console.log(cache_time);
+        const url = new URL(request.url);
+        if (url.pathname.startsWith('/api')) {    
+            if (request.method === 'OPTIONS') {
+                return new Response(null, {
+                    status: 200,
+                    statusText: 'OK',
+                    headers: {
+                        'content-type': 'application/json;charset=UTF-8',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': '*',
+                        'Access-Control-Allow-Credentials': 'true',
+                        'Access-Control-Allow-Headers': 'Content-Type',
+                        'Access-Control-Expose-Headers': '*'
+                    }
+                })
+            } else if (request.method === 'POST') {
+                return await handleRequest(request,env);
+            } else {
+                return new Response(null, {
+                    status: 405,
+                    statusText: 'Method Not Allowed',
+                })
+            }
+        }
+        // Otherwise, serve the static assets.
+        // Without this, the Worker will error and no assets will be served.
+        return env.ASSETS.fetch(request);
+    },
+  }
+
+
 
 
 async function gatherResponse(response) {
@@ -36,13 +71,14 @@ async function readRequestBody(request) {
     }
 }
 
-async function handleRequest(request) {
+async function handleRequest(request,env) {
+
     const timestamp = (Date.parse(new Date()))/1000;
-    const lasttime = await statuslive.get("statuslive_lasttime");
+    const lasttime = await env.statuslive.get("statuslive_lasttime");
     let results = '';
     let cache_tag = '';
     if (timestamp - lasttime <= cache_time) {
-        results = await statuslive.get("statuslive_json_cache");
+        results = await env.statuslive.get("statuslive_json_cache");
         cache_tag = 'Cache Hit - Time:'+lasttime;
     }else{
         const post_data = JSON.parse(await readRequestBody(request));
@@ -66,8 +102,8 @@ async function handleRequest(request) {
         }
 
         results = await JSON.stringify(results);
-        await statuslive.put("statuslive_json_cache", results, {expirationTtl: (cache_time < 60 ? 60 : cache_time)})
-        await statuslive.put("statuslive_lasttime", timestamp, {expirationTtl: (cache_time < 60 ? 60 : cache_time)})
+        await env.statuslive.put("statuslive_json_cache", results, {expirationTtl: (cache_time < 60 ? 60 : cache_time)})
+        await env.statuslive.put("statuslive_lasttime", timestamp.toString(), {expirationTtl: (cache_time < 60 ? 60 : cache_time)})
         cache_tag = 'Cache Miss - Time:'+timestamp;
     }
 
@@ -84,35 +120,3 @@ async function handleRequest(request) {
     }
     return new Response(results, response_init);
 }
-
-addEventListener('fetch', event => {
-    const request = event.request;
-    const url = new URL(request.url);
-
-    if (request.method === 'OPTIONS') {
-        event.respondWith(
-            new Response(null, {
-                status: 200,
-                statusText: 'OK',
-                headers: {
-                    'content-type': 'application/json;charset=UTF-8',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': '*',
-                    'Access-Control-Allow-Credentials': 'true',
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                    'Access-Control-Expose-Headers': '*'
-                }
-            })
-        );
-    } else if (request.method === 'POST') {
-        event.respondWith(handleRequest(request));
-    } else {
-        event.respondWith(
-            new Response(null, {
-                status: 405,
-                statusText: 'Method Not Allowed',
-            })
-        );
-    }
-
-});
